2d6ec27fd28038bb63c424b611cdb113fd816c30
Jimmy Bergström <me@kwarf.com>
2023-09-30 18:00:00 +0200

    Basic movement

---
+++ player/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+@onready var camera: Camera3D = get_viewport().get_camera_3d()
+@onready var mesh: MeshInstance3D = $Player
+const SPEED = 5.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Rotate the player to face the mouse.
+	var offset = -PI * 0.5
+	var screen_pos = camera.unproject_position(global_transform.origin)
+	var mouse_pos = get_viewport().get_mouse_position()
+	var angle = screen_pos.angle_to_point(mouse_pos)
+	mesh.rotation.y = -angle + offset
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
